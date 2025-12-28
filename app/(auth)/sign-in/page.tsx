dd6ead91cb95f6b43442a05b5480027f192cdd78
lironhefcode <lironheftman@gmail.com>
2025-12-28 20:55:58 +0200

    fixed coderabit countryfalg rerender

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
+const SignIn = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    defaultValues: { email: "", password: "" },
+    mode: "onBlur",
+  });
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error("Sign-ןמ error:", error);
+    }
+  };
+  return (
+    <>
+      <h1 className="form-title">Sign in</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 ">
+        <InputField
+          name="email"
+          type="email"
+          placeholder="2Qd7o@example.com"
+          label="Email"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: { value: /^\S+@\S+$/i, message: "Invalid email address" },
+          }}
+        />
+        <InputField
+          name="password"
+          type="password"
+          placeholder="enter your password"
+          label="Password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+          }}
+        />
+        <FooterLink
+          href="/sign-up"
+          linkText="Sign up"
+          text="Don't have an account?"
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Creating..." : "Sign Up"}
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default SignIn;
