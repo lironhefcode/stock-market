45c8739a2e99fb7489cf6e48d22de6f978209f08
lironhefcode <lironheftman@gmail.com>
2025-12-30 21:49:48 +0200

    added another chart to symbol page

--- app/(root)/[symbol]/page.tsx
+++ app/(root)/[symbol]/page.tsx
@@ -1,10 +1,12 @@
 import TradingViewWidget from "@/components/TradingViewWidget"
+import { Button } from "@/components/ui/button"
 import {
   SYMBOL_INFO_WIDGET_CONFIG,
   CANDLE_CHART_WIDGET_CONFIG,
   TECHNICAL_ANALYSIS_WIDGET_CONFIG,
   COMPANY_PROFILE_WIDGET_CONFIG,
   COMPANY_FINANCIALS_WIDGET_CONFIG,
+  BASELINE_WIDGET_CONFIG,
 } from "@/lib/constants"
 
 interface SymbolPageProps {
@@ -16,62 +18,74 @@ const SymbolPage = async ({ params }: SymbolPageProps) => {
   const scriptUrl = "https://s3.tradingview.com/external-embedding/embed-widget-"
 
   return (
-    <div className="flex min-h-screen home-wrapper">
-      <section className="grid w-full gap-8 home-section">
-        {/* Candle Chart Widget */}
-        <div className="md:col-span-1 xl:col-span-2">
-          <TradingViewWidget
-            config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
-            scriptUrl={scriptUrl + "advanced-chart.js"}
-            title={`${symbol.toUpperCase()} Chart`}
-            height={600}
-          />
-        </div>
-        {/* Symbol Info Widget */}
-        <div className="md:col-span-1 xl:col-span-1">
-          <TradingViewWidget
-            config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
-            scriptUrl={scriptUrl + "symbol-info.js"}
-            title={`${symbol.toUpperCase()} Overview`}
-            height={170}
-          />
-        </div>
-      </section>
+    <div className="min-h-screen home-wrapper">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+        {/* Left Column - First Section */}
+        <section className="flex flex-col gap-8">
+          {/* Symbol Info Widget */}
+          <div>
+            <TradingViewWidget
+              config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
+              scriptUrl={scriptUrl + "symbol-info.js"}
+              title={`${symbol.toUpperCase()} Overview`}
+              height={170}
+              className="custom-chart"
+            />
+          </div>
+          {/* Candle Chart Widget */}
+          <div>
+            <TradingViewWidget
+              className="custom-chart"
+              config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
+              scriptUrl={scriptUrl + "advanced-chart.js"}
+              height={600}
+            />
+          </div>
+          {/* Symbol Overview */}
+          <div>
+            <TradingViewWidget
+              className="custom-chart"
+              config={BASELINE_WIDGET_CONFIG(symbol)}
+              scriptUrl={scriptUrl + "advanced-chart.js"}
+              height={600}
+            />
+          </div>
+        </section>
 
-      <section className="grid w-full gap-8 home-section">
-        {/* Technical Analysis Widget */}
-        <div className="h-full md:col-span-1 xl:col-span-1">
-          <TradingViewWidget
-            config={TECHNICAL_ANALYSIS_WIDGET_CONFIG(symbol)}
-            scriptUrl={scriptUrl + "technical-analysis.js"}
-            title={`${symbol.toUpperCase()} Technical Analysis`}
-            height={400}
-            className="custom-chart"
-          />
-        </div>
+        {/* Right Column - Second Section */}
+        <section className="flex flex-col gap-8">
+          <Button className="w-full h-12 bg-yellow-400  font-semibold">add to watchlist</Button>
+          {/* Technical Analysis Widget */}
+          <div>
+            <TradingViewWidget
+              config={TECHNICAL_ANALYSIS_WIDGET_CONFIG(symbol)}
+              scriptUrl={scriptUrl + "technical-analysis.js"}
+              height={400}
+              className="custom-chart"
+            />
+          </div>
 
-        {/* Company Profile Widget */}
-        <div className="h-full md:col-span-1 xl:col-span-1">
-          <TradingViewWidget
-            config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)}
-            scriptUrl={scriptUrl + "symbol-profile.js"}
-            title={`${symbol.toUpperCase()} Company Profile`}
-            height={440}
-            className="custom-chart"
-          />
-        </div>
+          {/* Company Profile Widget */}
+          <div>
+            <TradingViewWidget
+              config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)}
+              scriptUrl={scriptUrl + "symbol-profile.js"}
+              height={440}
+              className="custom-chart"
+            />
+          </div>
 
-        {/* Company Financials Widget */}
-        <div className="h-full md:col-span-1 xl:col-span-1">
-          <TradingViewWidget
-            config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
-            scriptUrl={scriptUrl + "financials.js"}
-            title={`${symbol.toUpperCase()} Financials`}
-            height={464}
-            className="custom-chart"
-          />
-        </div>
-      </section>
+          {/* Company Financials Widget */}
+          <div>
+            <TradingViewWidget
+              config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
+              scriptUrl={scriptUrl + "financials.js"}
+              height={464}
+              className="custom-chart"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
