415e9853e92803c763ab6e7d107c1e7e6085a4b7
lironhefcode <lironheftman@gmail.com>
2026-01-19 17:43:51 +0200

    middlware prod change

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,19 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from "next/server"
+import { getSessionCookie } from "better-auth/cookies"
+import { getSession } from "@/lib/actions/auth.actions"
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  const sessionCookie = getSessionCookie(request)
 
   // Check cookie presence - prevents obviously unauthorized users
   if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.redirect(new URL("/sign-in", request.url))
+  }
+  const session = await getSession()
+  if (!session.success || !session.session?.user) {
+    // Session is invalid or expired → redirect
+    return NextResponse.redirect(new URL("/sign-in", request.url))
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|assets).*)",
-  ],
-};
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|assets).*)"],
+}
