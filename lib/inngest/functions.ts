0f8c25f03b0c0d0caac550ed6653c724a8405ea4
lironhefcode <lironheftman@gmail.com>
2026-02-15 22:03:58 +0200

    fixed resnding mails on mail cron job

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -46,14 +46,16 @@ export const sendSignUpEmail = inngest.createFunction({ id: "sign-up-email" }, {
   }
 })
 
+const triggers = process.env.NODE_ENV === "production" ? [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }] : [{ event: "app/send.daily.news" }]
 export const sendDailyNewsSummary = inngest.createFunction(
   {
     id: "daily-news-summary",
+    idempotency: `"daily-news-${new Date().toISOString().slice(0, 10)}"`,
     concurrency: {
       limit: 1, // Ensure only one instance runs at a time
     },
   },
-  [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
+  triggers,
   async ({ step }) => {
     // Step #1: Get all users for news delivery (deduplicated and validated)
     const users = await step.run("get-all-users", async () => {
