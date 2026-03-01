631007ba0985156c1cb60fdd6dcea153a32a1393
lironhefcode <lironheftman@gmail.com>
2026-03-01 17:16:30 +0200

    added that user who dont want to get daily email wont get them

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,26 +1,21 @@
-"use server";
+"use server"
 
-import { connectToDatabase } from "@/db/mongoose";
-import { count } from "console";
+import { connectToDatabase } from "@/db/mongoose"
+import { count } from "console"
 
-export const getAllUsersForNewsEmail = async (): Promise<
-  UserForNewsEmail[]
-> => {
+export const getAllUsersForNewsEmail = async (): Promise<UserForNewsEmail[]> => {
   try {
-    const mongoose = await connectToDatabase();
-    const db = mongoose.connection.db;
+    const mongoose = await connectToDatabase()
+    const db = mongoose.connection.db
     if (!db) {
-      console.error("Database connection not established");
-      throw new Error("Database connection not established");
-      return [];
+      console.error("Database connection not established")
+      throw new Error("Database connection not established")
+      return []
     }
     const user = await db
       .collection("user")
-      .find(
-        { email: { $exists: true, $ne: null } },
-        { projection: { email: 1, name: 1, id: 1, _id: 1, country: 1 } }
-      )
-      .toArray();
+      .find({ receiveDailyEmails: { $eq: true }, email: { $exists: true, $ne: null } }, { projection: { email: 1, name: 1, id: 1, _id: 1, country: 1 } })
+      .toArray()
     return user
       .filter((user) => user.email && user.name)
       .map((user) => ({
@@ -28,9 +23,9 @@ export const getAllUsersForNewsEmail = async (): Promise<
         email: user.email,
         name: user.name,
         country: user.country || "Not specified",
-      }));
+      }))
   } catch (error) {
-    console.error("Error fetching users for news email:", error);
-    return [];
+    console.error("Error fetching users for news email:", error)
+    return []
   }
-};
+}
